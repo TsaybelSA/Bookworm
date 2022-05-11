b99732c30bf8c6f02edc922f3c8508670460be29
TsaybelSA <tsaybelsa@gmail.com>
2022-05-11 11:00:21 +0600

    Finished RatingView.

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -18,11 +18,31 @@ struct RatingView: View {
 	var onImage = Image(systemName: "star.fill")
 	
 	var offColor = Color.gray
-	var onCOlor = Color.yellow
+	var onColor = Color.yellow
 	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		HStack {
+			if label.isEmpty == false {
+				Text(label)
+			}
+			
+			ForEach(1..<maximumRating + 1, id:\.self) { number in
+				image(for: number)
+					.foregroundColor(number > rating ? offColor : onColor)
+					.onTapGesture {
+						rating = number
+					}
+			}
+		}
     }
+	
+	func image(for number: Int) -> Image {
+		if number > rating {
+			return offImage ?? onImage
+		} else {
+			return onImage
+		}
+	}
 }
 
 struct RatingView_Previews: PreviewProvider {
